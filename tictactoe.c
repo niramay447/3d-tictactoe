6ac60eafa357f2351b7f0ab1895d9118e60c493a
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-09-07 18:07:54 +0530

    Third Commit

--- tictactoe.c
+++ tictactoe.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
+#include<stdlib.h>
+
 
 void printArray(int array[3][3][3]){
+//this function prints the board after each turn 
 for(int i=0;i<3;i++)
 {
 	for(int j=0;j<3;j++)
@@ -17,7 +20,11 @@ for(int i=0;i<3;i++)
 };
 }
 
+
 void createMagicCube(int array[3][3][3]){
+/*this function will create a magic cube using a formula 
+and a set of constants a, b, c for different values of i,j,k
+ in the range of 1 to 3*/
 int i, j, k;
 
 printf("____Magic Cube____\n\n");
@@ -46,6 +53,7 @@ for(int i=1;i<=3;i++)
 }
 
 void createboard(int array[3][3][3]){
+	//this function creates the board for tic tac toe and initialises all board position with 0
 	for(int i=0;i<3;i++)
 {
 	for(int j=0;j<3;j++)
@@ -59,18 +67,59 @@ void createboard(int array[3][3][3]){
 }
 
 int getUserInput(){
+	//this function scans the user input and returns the integer that user types
 	int userInp;
 	scanf("Type the position you want to mark:%d", userInp);
 	return userInp;
 }
 
-int getComputerInput(int compPosition, int userPosition, int userArray[], int compArray[]){
-	if compPosition == 0
-		return 14
-	else{
-		return 45-(userArray[userPosition]+userArray[userPosition])
-	} 
+int getComputerInput(int compPosition, int userPosition,int compArray[30] ,int userArray[30],int array[3][3][3] ){
+	//this function returns the integer value of the computer's move after checking for certain conditions
+	
+	/*if the array is empty(first move of the computer), 
+	computer places its first move at the center position
+	 on the board which is 14*/
+	if (compPosition == 0){
+		return 14;
+		exit
+	}
+	elif (compPosition == 1){
+		return 45-(userArray[userPosition]+userArray[userPosition]);
+		exit;
+	}
+	elif (checkComputerWin(compPosition,compArray,array) !=0){
+		return checkComputerWin(compPosition,compArray,array);
+	}
 }
+
+bool is_empty(int array[3][3][3], int position){
+/* this function takes the board and a integer position as the inputs
+and returns true if that position is empty*/
+}
+ 
+int checkComputerWin(int comp,int compArray[30], int array[3][3][3]){
+//This funciton returns true if the computer can win in the next move
+	 int move = 45-(compArray[comp]+compArray[comp-1]);
+	 if (is_empty(array,move)==1){
+		return move;
+	 }
+	 else{
+		 return 0;
+	 }
+};
+
+bool checkUserWin(int user, int array[30]){
+/*This function returns true 
+if the user can win in the next move so that position can be blocked*/
+	if is_empty(45-array[user]+array[user-1]){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+
 void playTTT(int array[3][3][3]){
     createboard(array);
 	printArray(array);
@@ -80,9 +129,12 @@ void playTTT(int array[3][3][3]){
 	int comp = 0;
 	int userCollinerLines = 0;
 	int compCollinearLines = 0;
-	userArray[user] = getUserInput();
-	compArray[comp] = getComputerInput();
+	while (userCollinerLines<10 or compCollinearLines<10){
+		userArray[user] = getUserInput();
+		compArray[comp] = getComputerInput();
 
+	}
+		
 }
 
 void main()
